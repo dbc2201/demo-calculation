1ff77b98208a4b432c9c86194f73ceb218d93167
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-20 13:11:07 +0530

    added method to divide two numbers

--- src/main/java/com/niit/bep/s1/demo/Main.java
+++ src/main/java/com/niit/bep/s1/demo/Main.java
@@ -20,4 +20,10 @@ public class Main {
         return firstNumber * secondNumber;
     }
 
+    public double divide(double dividend, double divisor) {
+        if (divisor == 0.0) {
+            return 0.0;
+        }
+        return dividend / divisor;
+    }
 }
