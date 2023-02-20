2f96199ffe0d2bdc5ea121491c0840d41aff41bd
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-20 13:18:13 +0530

    added test class

---
+++ src/test/java/com/niit/bep/s1/demo/MainTest.java
@@ -0,0 +1,32 @@
+package com.niit.bep.s1.demo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
